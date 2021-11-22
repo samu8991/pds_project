1e359d1d4d2dbb852790bd3833f9bf46c05ac41a
Samuele Paone <samu8991@gmail.com>
2021-11-22 17:30:39 +0100

    -Aggiunta file menu.h dove inserire tutte le funzione del menu - Corretto setup del CMakeLists.txt per usare le libreria filesystem e boost::options

--- main.cpp
+++ main.cpp
@@ -4,13 +4,6 @@
 
 int 
 main(){
-    po::options_description cmd_options("If the program is executed without "
-                                        "an input file, an interactive "
-                                        "version will run.\n"
-                                        "It is also possibile to run the program "
-                                        "without explicit indication, using the "
-                                        "following order: input, internal_representation, algorithm, threads[, trials].\n"
-                                        "e.g. GraphColoring INPUT_PATH 0 1 2 2, GraphColoring INPUT_PATH 1 1 1 2 5\n"
-                                        "Available options");
+    printf("Hello world\n");
     return EXIT_SUCCESS;
 }
